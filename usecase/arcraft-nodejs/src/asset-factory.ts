77aed4f76e9d61ad9b65b0af7a4a488570131da5
Umajyothichittiprolu <umajyothichittiprolu@gmail.com>
2025-06-18 20:08:09 +0530

    add asset Factory nodejs usecase

---
+++ usecase/arcraft-nodejs/src/asset-factory.ts
@@ -0,0 +1,14 @@
+import { Arc19, Arc3, Arc69, AssetFactory, CoreAsset } from "arcraft";
+
+
+async function main() {
+
+    const asset : CoreAsset | Arc3 | Arc69 | Arc19 = await AssetFactory.fromId(739833611, "mainnet");
+    // will return respective arc instance without manual initialization
+
+    const arcType = await AssetFactory.getArcType(739833611, "mainnet");
+    // arcType: arc3 or arc19 or arc69 or unknown
+    
+}
+
+main();
