40015123a24424cb9ccfc3ac32e004d96736c1b6
satishccy <satishchoudhari13579@gmail.com>
2025-11-15 02:02:36 +0530

    add usecase build

--- usecase/arcraft-frontend/vite.config.ts
+++ usecase/arcraft-frontend/vite.config.ts
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    nodePolyfills({
-      // To exclude specific polyfills, add them to this list.
-      // For example, if you don't want to polyfill the 'fs' module, add 'fs' to this list.
-      exclude: [],
-      // Whether to polyfill `global`.
-      globals: {
-        Buffer: true, // can also be 'build', 'dev', or false
-        global: true,
-        process: true,
-      },
-      // Whether to polyfill `node:` protocol imports.
-      protocolImports: true,
-    }),
   ],
   define: {
     global: 'globalThis',
